375b5f5474c7b626aac064e4b28deebe4b18ad6c
mehedynoman11 <mehedynoman11@gmail.com>
2025-01-06 08:58:43 +0600

    Sort String

---
+++ O_Sort_String.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main() {
+    int N;
+    cin >> N;
+    char S[N + 1]; 
+    cin.ignore(); 
+
+    for(int i=0;i<N;i++) {
+        cin >> S[i];
+    }
+
+    for(int i=0;i<N-1;i++) {
+        for(int j=i+1;j<N;j++) {
+            if (S[i] > S[j]) {
+                char tmp = S[i];
+                S[i] = S[j];
+                S[j] = tmp;
+            }
+        }
+    }
+
+    for(int i=0;i<N;i++) {
+        cout << S[i] ;
+    }
+    
+    return 0;
+}
+
+
+// #include <iostream>
+// #include <algorithm>
+// using namespace std;
+
+// int main() {
+//     int N;
+//     cin >> N; // Read the size of the string
+
+   
+    
+
+
+
+//     return 0;
+// }
