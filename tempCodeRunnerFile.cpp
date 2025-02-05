b5bf6e6afda2a85d678d9f3d3cb514a9cdf243cd
mehedynoman11 <mehedynoman11@gmail.com>
2025-02-05 15:52:37 +0600

    Range_sum_query1

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,18 @@
- "<<students[i].cls<<" "<<students[i].section<<" "<<students[i].id<<endl;
+#include <iostream>
+using namespace std;
+int main() {
+    string s;
+    cin >> s;
+    int freq[26] = {0};
+    for(int i=0;i<s.length();i++) {
+        char c = s[i];
+        freq[c - 'a']++;
+    }
+    for(char i='a';i<='z';i++) {
+        if (freq[i-'a'] > 0) {
+            cout << i << " : " << freq[i-'a'] << endl;
+        }
+    }
+
+    return 0;
+}
