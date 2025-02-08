0ada3e15b170a3c535353aa2a6e2c44b8b6ca0b9
mehedynoman11 <mehedynoman11@gmail.com>
2025-02-08 20:40:04 +0600

    binary search index

--- Z_Binary_Search.cpp
+++ Z_Binary_Search.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 int main()
 {
@@ -9,28 +10,30 @@ int main()
     {
         cin >> a[i];
     }
-
+    sort(a, a+n);
     while (q--)
     {
         int x;
         cin >> x;
         bool flag = false;
-        for (int i = 0; i < n; i++)
-        {
-            if (a[i] == x)
-            {
+        int l=0;
+        int r=n-1;
+        while(l<=r) {
+            int mid_index = (l+r)/2;
+            if (a[mid_index] == x) {
                 flag = true;
+                break;
+            }
+            if (x > a[mid_index]) {
+                l = mid_index + 1;
+            } else {
+                r = mid_index - 1;
             }
         }
 
-        if (flag == true)
-        {
-            cout << "found" << endl;
-        }
-        else
-        {
-            cout << "not found" << endl;
-        }
+        if (flag == true) cout << "found" << endl;
+        else cout << "not found" << endl;
+        
     }
 
     return 0;
